171c15406f82b5c8accd3389adab2fc75ae77f04
Unai Pastor <unai.pastor@orbitalads.io>
2018-08-09 22:23:29 +0200

    Added delete_multi function for batch deleting

--- dittostore/dittostore.py
+++ dittostore/dittostore.py
@@ -20,5 +20,21 @@ class DittoStore:
         [entity._save_offline(exclude_from_indexes=exclude_from_indexes) for entity in entities]
         client.put_multi([entity.get_raw_entity() for entity in entities])
 
+    def delete_multi(self, entities : List[BaseEntity], batch_size: int = 500):
+        client = datastore.Client(project=self.project)
+        delete_keys = []
+        for index, entity in enumerate(entities, 1):
+            delete_keys.append(entity)
+            if not index % batch_size:
+                self._delete_batch(client, delete_keys)
+                delete_keys = []
+        self._delete_batch(client, delete_keys)
+
+    def _delete_batch(self, client, keys: List[BaseEntity]):
+        batch = client.batch()
+        batch.begin()
+        [batch.delete(key) for key in keys]
+        batch.commit()
+
     def generate_key(self, kind: str, identifier: str, parent_key: Key = None):
         return datastore.Client(project=self.project).key(kind, identifier, parent=parent_key)
